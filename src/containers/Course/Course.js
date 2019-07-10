c089c099283ca6bf099e6aa30e57fbd8eaf10ca2
Alexandria <alexandriamlewis@me.com>
2019-07-10 12:07:09 -0400

    pass title as query params

--- src/containers/Course/Course.js
+++ src/containers/Course/Course.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 class Course extends Component {
+    state = {
+        courseTitle: ''
+    }
+    
+    componentDidMount () {
+        console.log(this.props);
+        const query = new URLSearchParams(this.props.location.search);
+        console.log(query);
+        // URLSearchParams allows us to create an iterator (array)
+        // .entries available on the URLSearchParams element
+        // we can now loop through it
+        for (let param of query.entries() ) {
+            console.log(param);
+            this.setState({courseTitle: param[1]})
+        }
+    }
+
     render () {
         return (
             <div>
-                <h1>{this.props.match.params.courseTitle}</h1>
+                <h1>{this.state.courseTitle}</h1>
                 <p>You selected the Course with ID: {this.props.match.params.courseId}</p>
                 {/* in a functional component would just be 'props.match.params.id' */}
             </div>
